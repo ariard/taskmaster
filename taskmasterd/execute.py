f2002a2ae6310bf75e6321b64d49e4b83e9a3df3
Antoine Riard <ariard@student.42.fr>
2017-04-14 18:50:25 +0200

    fuck excve

--- taskmasterd/execute.py
+++ taskmasterd/execute.py
@@ -6,7 +6,7 @@
 #    By: ariard <ariard@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/04/07 04:23:04 by ariard            #+#    #+#              #
-#    Updated: 2017/04/12 22:20:22 by ariard           ###   ########.fr        #
+#    Updated: 2017/04/14 17:47:36 by ariard           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,8 +21,9 @@ class Program:
             self.numprocs = int(parse.get(program, "numprocs"))
             self.autostart = parse.get(program, "autostart")
             self.autorestart = parse.get(program, "autorestart")
-            self.startsecs = parse.get(program, "startsecs")
-            self.startretries = parse.get(program, "startretries")
+            self.exitcodes = int(parse.get(program, "exitcodes"))
+            self.startsecs = int(parse.get(program, "startsecs"))
+            self.startretries = int(parse.get(program, "startretries"))
             self.stopsignal = parse.get(program, "stopsignal")
             self.stopwaitsecs = parse.get(program, "stopwaitsecs")
             self.stdout = parse.get(program, "stdout_logfile")
@@ -30,12 +31,12 @@ class Program:
             self.env = parse.get(program, "environnement")
             self.dir = parse.get(program, "directory")
             self.umask = parse.get(program, "umask")
+            self.status = "STARTING"
 
     def conf(self):
         DG(" conf") 
 
         try:
-            DG(self.dir)
             os.chdir(self.dir) 
         except:
             DG("log : couldn't chdir")
--- taskmasterd/monitor.py
+++ taskmasterd/monitor.py
@@ -6,7 +6,7 @@
 #    By: ariard <ariard@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/04/07 02:59:46 by ariard            #+#    #+#              #
-#    Updated: 2017/04/13 00:37:52 by ariard           ###   ########.fr        #
+#    Updated: 2017/04/14 18:50:06 by ariard           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,9 +19,10 @@ from execute import *
 from debug import *
 
 # Table Prog
-#   1 : command
-#   2 : autostart
-#   3 : autorestart
+#   0 : command
+#   1 : autostart
+#   2 : autorestart
+#   3 : exitcodes
 #   4 : startsecs
 #   5 : startretries
 #   6 : stopsignal
@@ -31,23 +32,51 @@ from debug import *
 #   10: env
 #   11: dir
 #   12: umask
-#   13: status
+#   13 : program
+
+table_prog = dict()
+
+def launch(program):
+    global table_prog
+
+    status = os.fork()
+    table_prog[status] = [ program.command, \
+        program.autostart, program.autorestart, program.exitcodes, \
+        program.startsecs, program.startretries, program.stopsignal, \
+        program.stopwaitsecs, program.stdout, program.stderr, \
+        program.env, program.dir, program.umask, program, program.status]
+
+    if status == 0:
+        DG("launch command")
+        program.conf()
+        try:
+            args = program.command.split(' ')
+            DG(args[0])
+            os.execv(args[0], args)
+        except:
+            DG("log : no such program")
+            sys.exit(-1)
 
 def check_exit(number, frame):
     while True:
         status = 0
         try:
             pid = os.waitpid(-1, 0)
+            program = table_prog[pid[0]]
+            if program[3] != pid[1] \
+            and program[2] == "true":
+                if program[5] > 0:
+                    program[13].startretries -= 1
+                    launch(program[13])
         except:
             break
-        print(pid[0])
+
 
 class Monitor:
     def __init__(self, config, list_sections):
         self.config = config
         self.list_programs = list()
         self.list_programs.extend(list_sections)
-        self.table_prog = dict()
         signal.signal(signal.SIGCHLD, check_exit)
 
     def launch_all(self):
@@ -55,27 +84,7 @@ class Monitor:
             self.program = Program(self.config, i)
             if self.program.autostart == "true":
                 while self.program.numprocs > 0:
-                    self.launch(self.program, self.program.numprocs)
+                    launch(self.program)
                     self.program.numprocs -= 1
-        for i,j  in self.table_prog.items():
-            print(i, j)
-    
-    def launch(self, program, numero):
-        status = os.fork()
-        self.table_prog[program.command + str(numero)] = [ program.command, \
-            program.autostart, program.autorestart, program.startsecs, program.startretries, \
-            program.stopsignal, program.stopwaitsecs, program.stdout, program.stderr, \
-            program.env, program.dir, program.umask, status ]
-        if status == 0:
-            DG("launch command")
-            program.conf()
-            
-#           log start
-            try:
-                args = program.command.split(' ')
-                DG(args[0])
-                os.execv(args[0], args)
-            except:
-                DG("log : no such program")
-                sys.exit(-1)
-
+#        for i,j  in table_prog.items():
+#            print(i, j)
