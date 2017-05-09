02f699facd92bfebd6f20b2ab17bcd88737bf53a
Antoine Riard <ariard@student.42.fr>
2017-05-10 01:25:51 +0200

    merge form pipe_attach + list of test needed

---
+++ README.d
@@ -0,0 +1,99 @@
+# 42-nazi 21sh
+## Taskmaster
+
+Value by defaults
+Launch a great number of shorts programs with high number of process
+Reload config a lot of times
+Hard configs
+Shutdown on multiclients
+
+
+Multi values for exitcodes
+Startsecs and exit before
+
+Fausse valeurs pour configs
+
+
+### starts jobs / keep alive / accurate status
+
+start / restart / status / stop 
+
+sample/start one programs true
+sample/start two programs true/false
+sample/start three programs true/false/true
+sample/start five programs true/true/false/true/true
+sample/start ten programs all true
+
+sample/start two program always crash
+sample/start big output
+Kill manually programs
+
+#n## shutdown
+
+Try a `shutdown` with a lot of process, check with px if it well killed
+
+### reload
+
+diff between hard conditions/soft conditions
+
+### loggging systems
+
+Try to delete logfile
+Modify logfile
+See if server events are locked
+
+##basic shell
+-history
+-completion
+-line editing
+
+### features configuration file
+
+- command : faux path / absence de path
+- numprocs : big number, 0 procs
+- autostart : true/false/fuck
+- autoresart : true/false/fuck/unexpected 
+- exitcodes : 0, 1, a lot of exitcodes, alphabetic values
+- startsecs : big number, 0, 1, 2
+- startretries : big number, medium number, 0, 5
+- stopsignal : admit value, trick the value 
+- stopwaitsecs : big number, 0, 1 , 5
+- stdout : try with a fifo, normal file, no file, /dev/null
+- stderr : try with a fifo, normal file, no file /dev/null
+- environnement : pwd, env cmd
+- direcotry : combine with output, bizaare directory
+- umask : create file, read file
+
+###bonus 
+
+#### desclation
+
+Launch the program as root then change the right
+
+#### Client / Server achitecture 
+
+Manually kill server
+Manaually kill client
+Launch multiclients, see syhncrhonization between them
+
+#### Facilites
+
+Password three times
+Change password
+
+Check mail in case of fatal or unexpcted behavior
+
+#### Attach 
+
+test input/output program
+test big output program
+test with a complex configuration then control great again process
+
+##### Special Bonus
+
+-keyword all
+-config cmd
+-log cmd
+-alert cmd
+-multithreading
+-multiclients
